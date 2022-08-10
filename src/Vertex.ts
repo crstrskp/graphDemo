e4965ce7ecfcda82502b86247bc6684b7234ebfa
crstrskp <39930580+crstrskp@users.noreply.github.com>
2022-08-11 01:11:05 +0200

    awdfsad

--- src/Vertex.ts
+++ src/Vertex.ts
@@ -14,17 +14,22 @@ export class Vertex
         this.visited = false; 
     }
 
-    public isAdjacent(v : Vertex) : boolean
-    {
-        this.connectedEdges.forEach((element) => {
-            // console.log("this element is connected to: ", element.start.label, " and: ", element.end.label)
-            if (element.start == v || element.end == v)
-            {
-                console.log("should be returning true; element: ", element.start.label,"->", element.end.label, "v: ", v.label);
-                return true;
-            }
-        });
+    // ---------- THIS METHOD IS DISCONTINUED -----------// 
+    // ---------- THIS METHOD IS DISCONTINUED -----------// 
+    // ---------- THIS METHOD IS DISCONTINUED -----------// 
+    // ---- can now be found in the GraphImpl class (areAdjacent(v,w);)-----//
+    
+    // public isAdjacent(v : Vertex) : boolean
+    // {
+    //     this.connectedEdges.forEach((element) => {
+    //         // console.log("this element is connected to: ", element.start.label, " and: ", element.end.label)
+    //         if (element.start == v || element.end == v)
+    //         {
+    //             console.log("should be returning true; element: ", element.start.label,"->", element.end.label, "v: ", v.label);
+    //             return true;
+    //         }
+    //     });
 
-        return false;
-    }
+    //     return false;
+    // }
 }
--- test/Utilities.test.ts
+++ test/Utilities.test.ts
@@ -8,5 +8,10 @@ describe('testing Utility.ts', () => {
     test('something else', () => {
         expect(jestTest2()).toBe(42);
     });
+
+
+    // test('awaitPromiseInMs - awaits a result within the provided time (ms) and returns a status', async () => {
+    //     expect(awaitPromiseInMs(3000).toBe(Promise)).toBe(Result.Failed);
+    // });
 });
 
