0d8ab58c7393f2ee35ca2b11443ca565d557f01c
crstrskp <jesper@tjorne.dk>
2022-09-03 12:45:35 +0200

    renamed ->all edge and vertex.  removed explicit dirEdge

--- src/GraphImpl.ts
+++ src/GraphImpl.ts
@@ -4,10 +4,10 @@ import { Vertex } from "./Vertex";
 
 export class GraphImpl implements IGraph
 {
-    getVertices(): Vertex[] {
+    getAllVertices(): Vertex[] {
         throw new Error("Method not implemented."); 
     }
-    getEdges(): Edge[] {
+    getAllEdges(): Edge[] {
         throw new Error("Method not implemented.");
     }
     getIncidentEdges(v: Vertex): Edge[] {
@@ -16,10 +16,24 @@ export class GraphImpl implements IGraph
     getOpposite(v: Vertex, e: Edge): Vertex {
         throw new Error("Method not implemented.");
     }
-    getEndVertices(e: Edge): Vertex[] {
+    getVertices(e: Edge): Vertex[] {
         throw new Error("Method not implemented.");
     }
     areAdjacent(v: Vertex, w: Vertex): boolean {
+    // public isAdjacent(v : Vertex) : boolean
+    // {
+    //     this.connectedEdges.forEach((element) => {
+    //         // console.log("this element is connected to: ", element.start.label, " and: ", element.end.label)
+    //         if (element.start == v || element.end == v)
+    //         {
+    //             console.log("should be returning true; element: ", element.start.label,"->", element.end.label, "v: ", v.label);
+    //             return true;
+    //         }
+    //     });
+
+    //     return false;
+    // }
+
         throw new Error("Method not implemented.");
     }
     insertVertex(o: any): Vertex {
@@ -28,14 +42,10 @@ export class GraphImpl implements IGraph
     insertEdge(v: Vertex, w: Vertex, o: any): Edge {
         throw new Error("Method not implemented.");
     }
-    insertDirectedEdge(start: Vertex, end: Vertex, o: any): Edge {
-        throw new Error("Method not implemented.");
-    }
     removeVertex(v: Vertex): undefined {
         throw new Error("Method not implemented.");
     }
     removeEdge(e: Edge): undefined {
         throw new Error("Method not implemented.");
     }
-    
 }
