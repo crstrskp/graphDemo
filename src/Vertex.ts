34a4fa98602b977648cf1ac79c590f018d770d32
crstrskp <jesper@tjorne.dk>
2022-09-03 12:47:10 +0200

    cleaned up vertex

--- src/Vertex.ts
+++ src/Vertex.ts
@@ -1,35 +1,34 @@
 import { Edge } from "./Edge";
+import { IVertex } from "./IVertex";
 
-export class Vertex
+export class Vertex implements IVertex
 {
     label : string; 
-    visited : boolean; // used for iterating via search algorithms
-    connectedEdges: Edge[];
+    visited : boolean;  // used for iterating via search algorithms
+    fee : number;      
+    object : any; 
+    // connectedEdges: Edge[];
 
-    setLabel(s : string) { this.label = s; }
+    public setLabel(s : string) { this.label = s; }
+    public getLabel() { return this.label; }
 
     constructor(label : string) {
-        this.connectedEdges = new Array<Edge>();
+        // this.connectedEdges = new Array<Edge>();
         this.label = label; 
         this.visited = false; 
+        this.fee = -1;
     }
 
-    // ---------- THIS METHOD IS DISCONTINUED -----------// 
-    // ---------- THIS METHOD IS DISCONTINUED -----------// 
-    // ---------- THIS METHOD IS DISCONTINUED -----------// 
-    // ---- can now be found in the GraphImpl class (areAdjacent(v,w);)-----//
-    
-    // public isAdjacent(v : Vertex) : boolean
-    // {
-    //     this.connectedEdges.forEach((element) => {
-    //         // console.log("this element is connected to: ", element.start.label, " and: ", element.end.label)
-    //         if (element.start == v || element.end == v)
-    //         {
-    //             console.log("should be returning true; element: ", element.start.label,"->", element.end.label, "v: ", v.label);
-    //             return true;
-    //         }
-    //     });
+    public updateCost() {
+        // find pool
+        // update price ^^ 
+            //timedAwait ?? this.object.UpdatePrice();
+        // set cost to price ^^ 
+            //this.cost = this.
+    }
 
-    //     return false;
-    // }
+    public getCost() { return this.fee; }
+
+    public getObject() { return this.object; }
+
 }
