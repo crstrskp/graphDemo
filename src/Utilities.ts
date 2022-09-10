5f1b745bb75c5118d677778f55b4c08c3e37393c
crstrskp <jesper@tjorne.dk>
2022-09-10 08:39:36 +0200

    [wip] removed unused test methods

--- src/Utilities.ts
+++ src/Utilities.ts
@@ -1,58 +1,12 @@
+import { resolve } from "mz/dns";
 import { Vertex } from "./Vertex";
 
 export class Utilities
 {
-    static jestTest() {
-        return 42;
-    }
-    
-    static getAllAdjacentVertices(v : Vertex) 
+    static async timedAwait(delegate : any, maxExecutionTime_ms : number)
     {
-        var inbound = Utilities.getAllVerticesLeadingTo(v);
-        var outbound = Utilities.getAllVerticesLeadingFrom(v);
-
-        var result = new Array<Vertex>();
-
-        inbound.forEach((element) => {
-            result.push(element);
+        return new Promise(resolve => {
+            setTimeout(() => delegate(), maxExecutionTime_ms);
         });
-
-        outbound.forEach(element => {
-            result.push(element);
-        });
-
-        return result;
     }
-
-    public static getAllVerticesLeadingTo(v : Vertex) 
-    {
-        var result = new Array<Vertex>(); 
-
-        v.connectedEdges.forEach((element) => 
-        {
-            if (element.end == v) 
-                result.push(element.start);
-        });
-
-        return result; 
-    }
-
-    public static getAllVerticesLeadingFrom(v : Vertex)
-    {
-        var result = new Array<Vertex>();
-
-        v.connectedEdges.forEach((element) => 
-        {
-            if (element.start == v)
-                result.push(element.end);
-        });
-
-        return result;
-    }
-
-}
-
-export function jestTest2()
-{
-    return 42;
 }
