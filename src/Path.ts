58b90f30abc3c19911763e8e08085b6c225f93a7
crstrskp <jesper@tjorne.dk>
2022-11-14 23:21:59 +0100

    Update Path.ts

--- src/Path.ts
+++ src/Path.ts
@@ -6,21 +6,16 @@ export class Path
     steps : (Edge|Vertex)[] = [];
 
     totalCost : number = 0; 
-    src : Vertex;
-
-    constructor(src : Vertex)
-    {
-        this.src = src; 
-        this.addStep(src);
-    }
 
     public addStep(step : Vertex | Edge)
     {
+        // if (this.steps.includes(step)) return;
         this.steps.push(step);
         this.calculateTotalCost(); 
     }
 
-    private calculateTotalCost() {
+    private calculateTotalCost() 
+    {
         var cost = 0; 
         
         this.steps.forEach((step) =>
@@ -29,10 +24,13 @@ export class Path
                 cost += step.getCost();
         });
 
-        
         this.totalCost = cost; 
     }
 
+    /** NOTE: alters collection!
+     * Removes the first element of the steps list. 
+     * @returns the removed element
+     */
     public next() : Edge | Vertex | undefined
     {
         return this.steps.shift();
@@ -48,4 +46,32 @@ export class Path
         return this.totalCost;
     }
 
+    /**NOTE: alters collection!
+     * mutates the <steps> array
+     */
+    public reverse()
+    {
+        this.steps.reverse();
+    }
+
+    public toString = () : string => {
+        var msg = "";
+
+        this.steps.forEach((element) => 
+        {
+            if (element instanceof Vertex)
+            {
+                msg += "Vertex: "+element.label+" cost: "+element.getCost()+"\n";
+            }
+            else if (element instanceof Edge)
+            {
+                msg += "Edge from: "+element.start.label+" to: "+element.end.label+" cost: "+ element.getCost()+".\n";
+            }
+            else {
+                msg += "unknow type: "+element+"\n";
+            }
+        })
+
+        return msg;
+    }
 }
