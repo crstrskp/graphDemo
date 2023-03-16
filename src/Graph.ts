8a9695978fd30cc3ccc4b2eff514918c88b11eaa
crstrskp <jesper@tjorne.dk>
2023-03-16 08:39:30 +0100

    exports module

---
+++ src/Graph.ts
@@ -0,0 +1,11 @@
+import { GraphImpl } from './GraphImpl';
+import { Vertex } from './Vertex';
+import { Path } from './Path';
+import { Edge } from './Edge';
+
+module.exports = {
+    Graph: GraphImpl,
+    Vertex : Vertex,
+    Edge : Edge,    
+    Path : Path, 
+}
