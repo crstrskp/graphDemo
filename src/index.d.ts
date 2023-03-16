3986ab7046c01385e1d5970a90c4ee584d000098
crstrskp <jesper@tjorne.dk>
2023-03-16 15:16:14 +0100

    v0.1.7

--- src/index.d.ts
+++ src/index.d.ts
@@ -6,4 +6,6 @@ import { Path as tPath } from './Path';
 declare type Path = tPath;
 import { Vertex as tVertex } from './Vertex';
 declare type Vertex = tVertex;
+
+
 export { Edge, GraphImpl, Path, Vertex };
