342babe1d8d39787c1658ebaee104ac0483153b1
crstrskp <jesper@tjorne.dk>
2025-07-12 14:02:27 +0200

    removed unused IPathBuilder

--- src/IPathBuilder.ts
+++
@@ -1,8 +0,0 @@
-import { Path } from "./Path";
-import { Vertex } from "./Vertex";
-
-export interface IPathBuilder
-{
-    // createPathBetweenVertices(src : Vertex, dest : Vertex) : Path;
-}
-    
